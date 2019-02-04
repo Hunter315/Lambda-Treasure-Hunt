85fcfb9ff1fb150a89aca6011e6cc9a53b955002
Hunter315 <hunter.mckay.smith@gmail.com>
2019-02-04 11:59:43 -0700

    display response from init request in browser

--- treasure-hunt/src/App.js
+++ treasure-hunt/src/App.js
@@ -1,35 +1,15 @@
 import React, { Component } from "react";
-import graphMap from "./components/graphMap";
 import "./App.css";
-import axios from "axios";
-import myToken from './tokens/secretTokens'
+import DisplayContainer from "./components/displayContainer";
+
 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // save state of room, items, directions, player
-    };
-  }
-
-  componentDidMount() {
-    
-    
-    let config = {
-      headers: { Authorization: myToken }
-    };
-    axios
-      .get("https://lambda-treasure-hunt.herokuapp.com/api/adv/init", config)
-      .then(res => { console.log(res) })
-      .catch(err => {
-        console.log(err)
-      });
-  }
+  
   render() {
     return (
       <div className="App">
-        
+        <DisplayContainer/>
       </div>
     );
   }
