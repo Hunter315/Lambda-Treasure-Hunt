775544f9bd21d391d9b40cdffd3da7285cbcdf75
Hunter315 <35641380+Hunter315@users.noreply.github.com>
2019-02-08 13:41:13 -0700

    tried to recreate the functions and styling i lost...

--- treasure-hunt/src/components/Traversal.js
+++ treasure-hunt/src/components/Traversal.js
@@ -1,10 +1,10 @@
 import React from "react";
-import DisplayContainer from "./DisplayContainer";
+import DisplayContainer from "./displayContainer";
 import myToken from "../tokens/secretTokens";
 import axios from "axios";
 import styled from "styled-components";
 import GraphMap from "./GraphMap";
-import data from './data.json'
+import data from "./data.json";
 // My personal token is config
 const config = {
   headers: { Authorization: myToken }
@@ -34,17 +34,23 @@ export default class Traversal extends React.Component {
       visited: new Set(),
       countVisited: 0,
       graphLoaded: false,
+      encumbrance: null,
+      speed: null,
+      strength: null,
+      gold: null,
+      inventory: [],
+      name: "Hunter",
+      visited: new Set()
     };
   }
   //====================== TRAVERSAL FUNCTIONS ======================
   componentDidMount() {
-  
     if (localStorage.hasOwnProperty("graph")) {
       let value = JSON.parse(localStorage.getItem("graph"));
       this.setState({ graph: value, graphLoaded: true });
     } else {
-      localStorage.setItem('graph', JSON.stringify(data));
-      let value = JSON.parse(localStorage.getItem('graph'));
+      localStorage.setItem("graph", JSON.stringify(data));
+      let value = JSON.parse(localStorage.getItem("graph"));
       this.setState({ graph: value, graphLoaded: true });
     }
     this.getInfo();
@@ -55,6 +61,41 @@ export default class Traversal extends React.Component {
       this.mapCoordinates();
     }
   }
+  randomExplore = async () => {
+    const { cooldown, graph, room_id, items } = this.state;
+    let exits = [...this.state.exits];
+    let randomRoom = Math.floor(Math.random() * exits.length);
+    let nextRoom = graph[room_id][1][exits[randomRoom]];
+    if (this.state.encumbrance >= 9) {
+      this.goToShop()
+        .then(() => {
+          this.sellTreasure();
+        })
+        .then(() => this.randomExplore);
+    } else if (items.length) {
+      this.getItem().then(() => this.randomExplore());
+    } else {
+      await this.timeoutFunc(1000 * cooldown);
+      this.flyToRooms(exits[randomRoom], nextRoom).then(() =>
+        this.randomExplore()
+      );
+    }
+  };
+
+  goToShop = async () => {
+    const path = this.bft(this.state.room_id, 1);
+    if (typeof path === "string") {
+      console.log(path);
+    } else {
+      for (let direction of path) {
+        console.log(direction);
+        for (let d in direction) {
+          await this.timeoutFunc(1000 * this.state.cooldown);
+          await this.flyToRooms(d, direction[d]);
+        }
+      }
+    }
+  };
   //----- INITIALIZE GRAPH -----
 
   //----- Travel -----
@@ -125,15 +166,26 @@ export default class Traversal extends React.Component {
       graph[prev_room_id][1][move] = id;
       graph[id][1][reverseDirection[move]] = prev_room_id;
     }
+    if (prev_room_id !== null) {
+      graph[prev_room_id][0].color = "#525959";
+      graph[id][0].color = "#7dcdbe";
+    } else {
+      graph[0][0].color = "#525959";
+      graph[id][0].color = "#7dcdbe";
+    }
     localStorage.setItem("graph", JSON.stringify(graph));
     return graph;
   };
-
+  timeoutFunc = async time => {
+    return new Promise(resolve => {
+      setTimeout(resolve, time);
+    });
+  };
   //---trying other method---
   traverseMap = () => {
     let count = 1;
     let unknownExits = this.getUnknownExits();
-    console.log("unknown exits", unknownExits)
+    console.log("unknown exits", unknownExits);
     if (unknownExits.length) {
       let move = unknownExits[0];
       this.travel(move);
@@ -142,7 +194,7 @@ export default class Traversal extends React.Component {
 
       if (typeof path === "string") {
       } else {
-        console.log("path",path)
+        console.log("path", path);
         for (let direction of path) {
           for (let d in direction) {
             setTimeout(() => {
@@ -175,13 +227,13 @@ export default class Traversal extends React.Component {
   mapLinks = () => {
     const { graph } = this.state;
     const setLinks = [];
-    for(let room in graph){
+    for (let room in graph) {
       for (let linkedRoom in graph[room][1]) {
         setLinks.push([graph[room][0], graph[graph[room][1][linkedRoom]][0]]);
       }
-    } 
-    this.setState({ allLinks: setLinks});
-  }
+    }
+    this.setState({ allLinks: setLinks });
+  };
   updateVisited = () => {
     let visited = new Set(this.state.set);
     for (let key in this.state.graph) {
@@ -253,23 +305,113 @@ export default class Traversal extends React.Component {
         console.log(err);
       });
   };
-  getItem = async () => {
-    let treasure = this.state.items;
 
-    try {
-      await axios({
+  getAllItems = async () => {
+    const { items, cooldown } = this.state;
+    for ( let item of items ){
+      await this.timeoutFunc(1000 * cooldown);
+      await this.getItem(item)
+    }
+    await this.timeoutFunc( 1000 * cooldown)
+  }
+  getItem = name => {
+     axios({
         method: "post",
         url: "https://lambda-treasure-hunt.herokuapp.com/api/adv/take/",
         headers: {
           Authorization: myToken
         },
         data: {
-          name: treasure
+          name
         }
+      }) .then(res => {
+        console.log(res.data);
+        this.setState(
+          {
+            messages: res.data.messages,
+            items: res.data.items,
+            players: res.data.players,
+            cooldown: res.data.cooldown
+          },
+          () => this.timeoutFunc(1000 * res.data.cooldown).then(() => this.getStatus())
+        );
+      })
+
+      .catch(err => {
+        console.log('There was an error.');
+        console.dir(err);
       });
-    } catch (err) {
-      console.log(err);
-    }
+  };
+  prayToShrine = () => {
+    axios({
+      method: 'post',
+      url: 'https://lambda-treasure-hunt.herokuapp.com/api/adv/pray/',
+      headers: {
+        Authorization: myToken
+      }
+    })
+      .then(res => {
+        console.log(res.data);
+        this.setState({ messages: [...res.data.messages] }, () =>
+          this.getStatus()
+        );
+      })
+      .catch(err => {
+        console.log('There was an error.');
+        console.dir(err);
+      });
+  };
+
+  changeName = () => {
+    axios({
+      method: 'post',
+      url: 'https://lambda-treasure-hunt.herokuapp.com/api/adv/change_name/',
+      headers: {
+        Authorization: myToken
+      },
+      data: {
+        name: 'Hunter'
+      }
+    })
+      .then(res => {
+        console.log(res.data);
+        this.setState({ messages: [...res.data.messages] }, () =>
+          this.getStatus()
+        );
+      })
+      .catch(err => {
+        console.log('There was an error.');
+        console.dir(err);
+      });
+  };
+
+  getStatus = () => {
+    axios({
+      method: 'post',
+      url: 'https://lambda-treasure-hunt.herokuapp.com/api/adv/status/',
+      headers: {
+        Authorization: myToken
+      }
+    })
+      .then(res => {
+        console.log(res.data);
+
+        this.setState(prevState => ({
+          name: res.data.name,
+          cooldown: res.data.cooldown,
+          encumbrance: res.data.encumbrance,
+          strength: res.data.strength,
+          speed: res.data.speed,
+          gold: res.data.gold,
+          inventory: [...res.data.inventory],
+          status: [...res.data.status],
+          errors: [...res.data.errors]
+        }));
+      })
+      .catch(err => {
+        console.log('There was an error.');
+        console.dir(err);
+      });
   };
   handleChange = event => {
     event.preventDefault();
@@ -306,14 +448,68 @@ export default class Traversal extends React.Component {
         }
       }
     }
-    return 'doesnt exist';
+    return "doesnt exist";
   };
+  flyToRooms = async (move, next_room_id = null) => {
+    let data;
+    if (next_room_id !== null) {
+      data = {
+        direction: move,
+        next_room_id: next_room_id.toString()
+      };
+    } else {
+      data = {
+        direction: move
+      };
+    }
+    try {
+      const response = await axios({
+        method: 'post',
+        url: `https://lambda-treasure-hunt.herokuapp.com/api/adv/fly/`,
+        headers: {
+          Authorization: myToken
+        },
+        data
+      });
 
+      let previous_room_id = this.state.room_id;
+
+      //   Update graph
+      let graph = this.updateGraph(
+        response.data.room_id,
+        this.parseCoordinates(response.data.coordinates),
+        response.data.exits,
+        previous_room_id,
+        move
+      );
+      this.setState({
+        room_id: response.data.room_id,
+        coords: this.parseCoordinates(response.data.coordinates),
+        exits: [...response.data.exits],
+        cooldown: response.data.cooldown,
+        messages: response.data.messages,
+        description: response.data.description,
+        title: response.data.title,
+        players: response.data.players,
+        items: response.data.items,
+        graph
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.log('Something went wrong moving...');
+      console.dir(error);
+      this.setState({
+        cooldown: error.response.data.cooldown,
+        messages: [...error.response.data.errors]
+      });
+      throw error;
+    }
+  };
   handleClick = () => {
     this.traverseMap();
   };
   render() {
-    const { graph } = this.state
+    const { graph } = this.state;
     // console.log("graph", graph)
     return (
       <React.Fragment>
@@ -326,8 +522,20 @@ export default class Traversal extends React.Component {
         <Button onClick={() => this.travel("w")}>West</Button>
         <Button onClick={() => this.handleClick()}>AutoTraverse</Button>
         <Button onClick={() => this.getItem()}>Pick Up Treasure</Button>
-        { graph ? <GraphMap coordinates={this.state.allCoordinates} links={this.state.allLinks}/> : <div><p>graph loading</p></div> }
-        
+        <Button onClick={() => this.getStatus()}>Get Status</Button>
+        <Button onClick={() => this.randomExplore()}>Random Travels</Button>
+
+
+        {graph ? (
+          <GraphMap
+            coordinates={this.state.allCoordinates}
+            links={this.state.allLinks}
+          />
+        ) : (
+          <div>
+            <p>graph loading</p>
+          </div>
+        )}
 
         <form>
           <label>
